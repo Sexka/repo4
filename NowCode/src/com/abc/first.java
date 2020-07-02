704ef5135eb92b3bdfcfbe88fe7d3a9e7f8b1fc9
Rika <1316379832@qq.com>
2020-07-03 01:33:38 +0800

    222222222

--- NowCode/src/com/abc/first.java
+++ NowCode/src/com/abc/first.java
@@ -3,6 +3,19 @@ package com.abc;
 public class first {
     public static void main(String[] args) {
         System.out.println("abc");
+        System.out.println("abc");
+        System.out.println("abc");
+        System.out.println("abc");
+        System.out.println("abc");
+        System.out.println("abc");
+        System.out.println("abc");
+        System.out.println("abc");
+        System.out.println("abc");
+        System.out.println("abc");
+        System.out.println("abc");
+        System.out.println("abc");
+        System.out.println("abc");
+        System.out.println("abc");
 
     }
 }
